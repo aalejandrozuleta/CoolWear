c9371b71b5148176220502b00f936475ad086b07
aalejandrozuleta <jhoitan266@gmail.com>
2023-12-06 14:03:38 -0500

    fix: 'night mode'

--- src/components/layouts/nightMode/NightMode.jsx
+++ src/components/layouts/nightMode/NightMode.jsx
@@ -1,33 +1,39 @@
-import  { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMoon, faSun } from "@fortawesome/free-solid-svg-icons";
 
 const NightModeToggle = () => {
-    const [isNightMode, setIsNightMode] = useState(false);
+  const storedNightMode = localStorage.getItem("nightMode");
+  const [isNightMode, setIsNightMode] = useState(
+    storedNightMode ? JSON.parse(storedNightMode) : false
+  );
 
-    const toggleNightMode = () => {
-        setIsNightMode(!isNightMode);
-    };
+  const toggleNightMode = () => {
+    setIsNightMode(!isNightMode);
+  };
 
-    useEffect(() => {
-        const body = document.querySelector("body");
-        if (isNightMode) {
-            body.classList.add("night-mode");
-        } else {
-            body.classList.remove("night-mode");
-        }
-    }, [isNightMode]);
+  useEffect(() => {
+    const body = document.querySelector("body");
+    if (isNightMode) {
+      body.classList.add("night-mode");
+    } else {
+      body.classList.remove("night-mode");
+    }
 
-    return (
-        <div
-            className={`night-mode-toggle ${isNightMode ? "active" : ""}`}
-            onClick={toggleNightMode}
-        >
-            <span className="toggle-icon">
-                <FontAwesomeIcon icon={isNightMode ? faMoon : faSun} />
-            </span>
-        </div>
-    );
+    // Almacena el estado en localStorage
+    localStorage.setItem("nightMode", JSON.stringify(isNightMode));
+  }, [isNightMode]);
+
+  return (
+    <div
+      className={`night-mode-toggle ${isNightMode ? "active" : ""}`}
+      onClick={toggleNightMode}
+    >
+      <span className="toggle-icon">
+        <FontAwesomeIcon icon={isNightMode ? faMoon : faSun} />
+      </span>
+    </div>
+  );
 };
 
 export default NightModeToggle;
