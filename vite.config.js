e6be821526aee5f8fe7224a60748d060c4ff5719
aalejandrozuleta <jhoitan266@gmail.com>
2023-12-06 17:02:33 -0500

    update 2.0

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,22 @@
 // vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'http://localhost:5174', 
+      "/api": "http://localhost:5174",
     },
   },
   build: {
-    outDir: 'dist',
+    outDir: "dist",
     assetsInclude: [
-      'src/server.js',
-      'src/server/Api.jsx',
-      'src/server/controller.js',
-      'src/server/bd.js',
-      'src/server/routes.js',
-      
+      "src/server.js",
+      "src/server/Api.jsx",
+      "src/server/controller.js",
+      "src/server/bd.js",
+      "src/server/routes.js",
     ],
   },
 });
