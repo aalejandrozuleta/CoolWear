67742f9160cf3a07ef3ac78a6fe940fa15ac1532
aalejandrozuleta <jhoitan266@gmail.com>
2023-12-07 02:39:34 -0500

    deploy 1.12

--- server/Api.jsx
+++ server/Api.jsx
@@ -2,63 +2,63 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 // validation login true
 export const loginUser = (email, password) => {
-    return axios.post("/api/loginUser", {
-        email_user: email,
-        password_user: password,
-    });
+  return axios.post("/api/loginUser", {
+    email_user: email,
+    password_user: password,
+  });
 };
-
 
 export const handleLogout = () => {
-    axios.get('/api/logout') 
-        .then(() => {
-            localStorage.setItem("isLoggedIn", "false");
-            window.location.href = '/login';
-        })
-        .catch((error) => {
-            console.error('Error al cerrar sesión: ', error);
-        });
+  axios
+    .get("/api/logout")
+    .then(() => {
+      localStorage.setItem("isLoggedIn", "false");
+      window.location.href = "/login";
+    })
+    .catch((error) => {
+      console.error("Error al cerrar sesión: ", error);
+    });
 };
 
-  export function useFetchUserData() {
-    const [user, setUser] = useState({
-      name_user: "",
-      phone_user: "",
-      email_user: "",
-      password_user: ""
-    });
-  
-    useEffect(() => {
-      axios.get('/api/getUserData')
-        .then((response) => {
-          setUser(response.data);
-        })
-        .catch((error) => {
-          console.error('Error al obtener datos del usuario: ', error);
-        });
-    }, []);
-  
-    return user;
-  }
+export function useFetchUserData() {
+  const [user, setUser] = useState({
+    name_user: "",
+    phone_user: "",
+    email_user: "",
+    password_user: "",
+  });
 
-  export const handleUpdateUser = (user) => {
-    axios.post('/api/updateUser', user)
+  useEffect(() => {
+    axios
+      .get("/api/getUserData")
       .then((response) => {
-        console.log('Datos del usuario actualizados con éxito.');
+        setUser(response.data);
       })
       .catch((error) => {
-        console.error('Error al actualizar datos del usuario: ', error);
+        console.error("Error al obtener datos del usuario: ", error);
       });
-  };
-  
+  }, []);
 
-  export const getProducts = async () => {
-    try {
-      const response = await axios.get("/api/getProducts"); 
-      return response.data;
-    } catch (error) {
-      console.error("Error al obtener productos: ", error);
-      throw error;
-    }
-  };
-  
+  return user;
+}
+
+export const handleUpdateUser = (user) => {
+  axios
+    .post("/api/updateUser", user)
+    .then((response) => {
+      console.log("Datos del usuario actualizados con éxito.");
+    })
+    .catch((error) => {
+      console.error("Error al actualizar datos del usuario: ", error);
+    });
+};
+
+export const getProducts = async () => {
+  try {
+    const response = await axios.get("/api/getProducts");
+    return response.data;
+  } catch (error) {
+    console.error("Error al obtener productos: ", error);
+    throw error;
+  }
+};
--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'https://bd-sql-017887c5bc0a.herokuapp.com/',
+      '/api': 'https://bd-sql-017887c5bc0a.herokuapp.com',
     },
   },
 });
